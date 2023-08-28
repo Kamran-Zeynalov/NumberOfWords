e7624889892e0615504a83d019843e7f25ee5f7c
Kamran Z <kamraneze@code.edu.az>
2023-08-28 10:59:42 +0400

    Number of words in a string

---
+++ NumberOfWords/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NumberOfWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Salam eziz insanlar";
+            string[] words = sentence.Split(new[] { ' ', '\t', '\n', '\r' });
+            Console.WriteLine(words.Length);
+        }
+    }
+}
